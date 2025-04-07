3a5fd5370b07066dbd6297581958eb74d5cdd739
Monan Bertoluci <monan@weareglobals.com>
2025-04-07 16:08:31 -0300

    fix: remove configurações complexas para resolver erro de build

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,1 @@
-module.exports = ({ env }) => ({
-  'users-permissions': {
-    config: {
-      jwtSecret: env('JWT_SECRET'),
-      jwt: {
-        expiresIn: '7d',
-      },
-    },
-  },
-});
+module.exports = () => ({});
