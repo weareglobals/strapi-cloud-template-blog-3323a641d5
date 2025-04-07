7d3c7c9d39cb3020fcb6654114ff0166ed55ee20
Monan Bertoluci <monan@weareglobals.com>
2025-04-07 18:10:53 -0300

    feat: adiciona controllers e services para todas as APIs

---
+++ src/api/success-case/controllers/success-case.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  success-case controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::success-case.success-case'); 
---
+++ src/api/success-case/services/success-case.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * success-case service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::success-case.success-case'); 
