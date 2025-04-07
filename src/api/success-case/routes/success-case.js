cf26e20e33928b73f6d4238103b6f64f30f9ef49
Monan Bertoluci <monan@weareglobals.com>
2025-04-07 18:02:24 -0300

    feat: adiciona arquivos de rotas para todas as APIs

---
+++ src/api/success-case/routes/success-case.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * success-case router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::success-case.success-case'); 
